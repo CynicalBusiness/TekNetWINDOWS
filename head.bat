f61d05bcda0db0b91f5e13acd325b132c7e01132
Matthew Struble <Matt@Mayateck.net>
2013-08-12 22:55:47 -0700

    Scraped "terminal.bat", added to head.

--- head.bat
+++ head.bat
@@ -6,7 +6,8 @@ echo "  | |  |  _|  |   ( | |\  ||  _|   | |  "
 echo "  |_|  |____| |_|\_\|_| \_||____|  |_|  "
 echo "#===#------------------------------#===#"
 IF "%*"=="" (
-	echo TekNet initialized.
+	echo Terminal Ready. Welcome to TekNet.
+	echo Enter "commands" for a list of commands.
 ) ELSE (
 	echo %*
 )
