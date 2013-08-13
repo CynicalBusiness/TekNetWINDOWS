4533f4f32db15e1e64ca64bdf17978a85256ee71
Matthew Struble <Matt@Mayateck.net>
2013-08-12 23:22:10 -0700

    Added a command list thus far.

---
+++ commands.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+echo Alphabetical TekNet Command List
+echo #===#------------------------#===#
+echo BOOTSVR     - Boots the requested server. (Only works on a TekNet machine.)
+echo CDRIVE      - Returns to the C:\WINDOWS\system32 directory. Add the parameter "return" to jump back to TekNet
+echo COMMANDS    - Echoes this help file.
+echo GIT         - Pushes or pulls GIT repos from/to TekNet defaults or the specified directory.
+echo HEAD        - Echoes the head with an optional custom message.
+echo #===#------------------------#===#
+
