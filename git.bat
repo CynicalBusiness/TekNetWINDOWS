efde98775af40cf562dcc73f7b7603e9e6be9d7d
Matthew Struble <Matt@Mayateck.net>
2013-08-12 22:55:29 -0700

    Delete to remove unwanted files.

--- git.bat
+++ git.bat
@@ -2,11 +2,13 @@
 echo Attempting GIT request "%1".
 echo #--------------------------#
 IF "%1"=="pull" (
+	del "D:\core\prog"
 	copy "D:\core\git\TekNetWINDOWS" "D:\core\prog"
 	echo #--------------------------#
 	echo Pulled from GIT.
 ) ELSE ( 
 	IF "%1"=="push" (
+		del "D:\core\git\TekNetWINDOWS"
 		copy "D:\core\prog" "D:\core\git\TekNetWINDOWS"
 		echo #--------------------------#
 		echo Pushed to GIT.
