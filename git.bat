d86332325c85842790d3af9b0512e44c9aebd42b
Matthew Struble <Matt@Mayateck.net>
2013-08-12 22:22:21 -0700

    Added line breakers.

--- git.bat
+++ git.bat
@@ -1,11 +1,14 @@
 @ECHO OFF
 echo Attempting GIT request "%1".
+echo #--------------------------#
 IF "%1"=="pull" (
 	copy "D:\core\git\TekNetWINDOWS" "D:\core\prog"
+	echo #--------------------------#
 	echo Pulled from GIT.
 ) ELSE ( 
 	IF "%1"=="push" (
 		copy "D:\core\prog" "D:\core\git\TekNetWINDOWS"
+		echo #--------------------------#
 		echo Pushed to GIT.
 	) ELSE (
 		echo Unknown git requester command.
