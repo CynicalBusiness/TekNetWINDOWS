ef9b085487abd362121443883436b43b1f8867bc
Matthew Struble <Matt@Mayateck.net>
2013-08-12 23:12:01 -0700

    Added some GIT push and pull stuff.

--- git.bat
+++ git.bat
@@ -2,14 +2,24 @@
 echo Attempting GIT request "%1".
 echo #--------------------------#
 IF "%1"=="pull" (
-	del "D:\core\prog"
-	copy "D:\core\git\TekNetWINDOWS" "D:\core\prog"
+	IF "%2"=="" (
+		del "D:\core\prog"
+		copy "D:\core\git\TekNetWINDOWS" "D:\core\prog"
+	) ELSE (
+		del %3
+		copy %2 %3
+	)
 	echo #--------------------------#
 	echo Pulled from GIT.
 ) ELSE ( 
 	IF "%1"=="push" (
-		del "D:\core\git\TekNetWINDOWS"
-		copy "D:\core\prog" "D:\core\git\TekNetWINDOWS"
+		IF "%2"=="" (
+			del "D:\core\git\TekNetWINDOWS"
+			copy "D:\core\prog" "D:\core\git\TekNetWINDOWS"
+		) ELSE (
+			del %3
+			copy %2 %3
+		)
 		echo #--------------------------#
 		echo Pushed to GIT.
 	) ELSE (
